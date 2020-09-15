cb560ef1233036bf44531a3d42d92647a95d2212
Harbrus <harbrus@outlook.com>
2020-09-15 13:51:41 +0100

    Initial commit

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+    private Vector3 followPos;
+
+    [SerializeField] GameObject targetObj, playerObj;
+    [SerializeField] Transform obstruction;
+    [SerializeField] GameObject cameraObj;
+    [SerializeField] float cameraSpeed = 120.0f;
+    [SerializeField] float clampAngleMin = -15;
+    [SerializeField] float clampAngleMax = 40;
+    [SerializeField] float inputSensitivity = 150.0f;
+    [SerializeField] float camDistanceXToPlayer;
+    [SerializeField] float camDistanceYToPlayer;
+    [SerializeField] float camDistanceZToPlayer;
+    [SerializeField] float mouseX;
+    [SerializeField] float mouseY;
+    [SerializeField] float finalInputX;
+    [SerializeField] float finalInputZ;
+    [SerializeField] float smoothSpeed; // if lerp used
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        obstruction = targetObj.transform;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraControl();
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    private void CameraControl()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, clampAngleMin, clampAngleMax);
+
+        playerObj.transform.rotation = Quaternion.Euler(0, rotY, 0);
+        transform.rotation = Quaternion.Euler(rotX, rotY, 0); // consider lerp to do it slower.
+
+    }
+
+    private void CameraUpdater()
+    {
+        // set the target object to follow
+        Transform target = targetObj.transform;
+
+        //move towards the game object that is the target
+        float step = cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonPlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ThirdPersonPlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 6f;
+    [SerializeField] float jumpHeight = 2f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float groundDistance = 0.2f;
+    [SerializeField] LayerMask ground;
+    
+
+    private Rigidbody playerRb;
+    private CharacterController _controller;
+    private Transform _groundChecker;
+    private Vector3 _velocity;
+    private bool isGrounded = true;
+
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _groundChecker = transform.GetChild(0);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        // check if grounded
+        isGrounded = Physics.CheckSphere(_groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
+        if (isGrounded && _velocity.y < 0)
+            _velocity.y = 0f;
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = transform.TransformDirection(move);
+        _controller.Move(move * Time.deltaTime * speed);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            _velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //// add gravity
+        _velocity.y += gravity * Time.deltaTime;
+
+        _controller.Move(_velocity * Time.deltaTime);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Walls"))
+        {
+            playerRb.velocity = Vector3.zero;
+        }
+    }
+}
