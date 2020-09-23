186d033b2fec0146a8feb6da4b3fb2f01ec7ad03
Harbrus <harbrus@outlook.com>
2020-09-23 10:46:47 +0100

    Game Manager and Analytics Recorder
    Started to work on these two scripts.

---
+++ Assets/Scripts/AnalyticsRecorder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+// A class to record data into UnityAnalytics when custom Events are fired.
+public class AnalyticsRecorder : MonoBehaviour
+{
+    // Recyclable dictionary for custom events
+    private static Dictionary<string, object> dict = new Dictionary<string, object>();
+
+    private void Awake()
+    {
+        GameManager.Instance.levelStartEvent += OnLevelStart;
+        GameManager.Instance.levelStartEvent += OnRespwan;
+        GameManager.Instance.levelStartEvent += OnLevelComplete;
+    }
+
+    public void OnLevelStart(string recordName, object recordObject) { }
+
+    public void OnRespwan(string recordName, object recordObject) { }
+
+    public void OnLevelComplete(string recordName, object recordObject) { }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Random used for level id generation 
+    private static System.Random random = new System.Random();
+    private string levelID = "";
+    // Game manager instance used for the singleton
+    protected static GameManager _instance;
+    // Time at the start of the level
+    protected DateTime _started;
+    // Time at the respawn
+    protected DateTime _fromLastRespawn;
+    // Player prefab istance
+    [SerializeField] private GameObject _playerPrefab;
+    /// the elapsed time since the start of the level
+    public System.TimeSpan GlobalTimer { get { return DateTime.UtcNow - _started; } }
+    // the elapsed time since the last respawn
+    public System.TimeSpan RespwanTimer { get { return DateTime.UtcNow - _fromLastRespawn; } }
+
+    public event Action<string, object> levelStartEvent;
+    public event Action<string, object> respawnEvent;
+    public event Action<string, object> levelCompleEvent;
+
+    // Singleton to access the GameManager from other classes
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject gameManager = new GameObject();
+                    _instance = gameManager.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    // Awake is called before anything else
+    private void Awake()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        if (_instance == null)
+        {
+            //If I am the first instance, make me the Singleton
+            _instance = this as GameManager;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            //If a Singleton already exists and you find
+            //another reference in scene, destroy it!
+            if (this != _instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        Initialization();
+    }
+
+    // initiliase any required components and variables at Start
+    protected virtual void Initialization()
+    {
+        _started = DateTime.UtcNow;
+        levelID = LevelID(5);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Generate random level id
+    public static string LevelID(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
